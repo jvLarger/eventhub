80f38c019e3751528012674ffae49681a27e3a99
João Victor <larger13@hotmail.com>
2023-04-30 14:46:53 -0300

    Criando a primeira entidade

---
+++ src/main/java/com/jlarger/eventhub/entities/Arquivo.java
@@ -0,0 +1,101 @@
+package com.jlarger.eventhub.entities;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+import org.hibernate.annotations.Type;
+import org.hibernate.type.descriptor.jdbc.BinaryJdbcType;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "arquivo")
+public class Arquivo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Basic(fetch = FetchType.LAZY)
+    private byte[] arquivo;
+	
+	@Column(nullable = false, length = 255)
+	private String nome;
+	
+	@Column(nullable = true, length = 255)
+	private String descricao;
+	
+	public Arquivo() {
+	}
+
+	public Arquivo(Long id, byte[] arquivo, String nome, String descricao) {
+		super();
+		this.id = id;
+		this.arquivo = arquivo;
+		this.nome = nome;
+		this.descricao = descricao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public byte[] getArquivo() {
+		return arquivo;
+	}
+
+	public void setArquivo(byte[] arquivo) {
+		this.arquivo = arquivo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.hashCode(arquivo);
+		result = prime * result + Objects.hash(descricao, id, nome);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Arquivo other = (Arquivo) obj;
+		return Arrays.equals(arquivo, other.arquivo) && Objects.equals(descricao, other.descricao)
+				&& Objects.equals(id, other.id) && Objects.equals(nome, other.nome);
+	}
+	
+}
